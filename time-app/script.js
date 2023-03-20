d5e0457a9f9bb804032f4c8b87bb1f41b15762dd
taichi-tech <tbetchaku787@gmail.com>
2023-03-20 13:17:29 +0900

    3/21 アナログ時計の実装完了 => テーマチェンジトグルの実装とタイマーの実装をすべし

--- time-app/script.js
+++ time-app/script.js
@@ -1,5 +1,6 @@
 const hambergerIcon = document.querySelector(".nav-toggle");
 const nav = document.querySelector("nav");
+const content = document.querySelector(".content");
 let navClick = false;
 
 hambergerIcon.addEventListener("click", () => {
@@ -8,19 +9,28 @@ hambergerIcon.addEventListener("click", () => {
     if (navClick === true) {
         hambergerIcon.className += " show";
         nav.className += " show";
+        content.className += " show";
     } else {
         hambergerIcon.className = "nav-toggle";
         nav.className = "nav";
+        content.className = "content";
     }
+    
 });
 
-const showDay = document.querySelector(".day-info");
-const showHour = document.querySelector(".hour-info");
-const weekChars = [ "日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日" ];
+//テーマチェンジトグル
+themeToggle.addEventListener("checked", ()=>{
+    
+})
+
 
 const realTime = setInterval(() => {
+    const showDay = document.querySelector(".day-info");
+    const showHour = document.querySelector(".hour-info");
+    const weekChars = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"];
+
     const now = new Date();
-    const timeInfo = {
+    timeInfo = {
         wDay: now.getDay(),
         year: now.getFullYear(),
         month: now.getMonth(),
@@ -29,18 +39,31 @@ const realTime = setInterval(() => {
         min: now.getMinutes(),
         sec: now.getSeconds(),
     };
-    wChars =  weekChars[timeInfo.wDay];
-    hourMinSec = timeInfo.hour + '時' + timeInfo.min + '分' + timeInfo.sec + '秒';
-    yearMonthDate = timeInfo.year + "年" + timeInfo.month + "月" + timeInfo.date + "日" + " (" + wChars + ')';
+    wChars = weekChars[timeInfo.wDay];
+    hourMinSec = timeInfo.hour + "時" + timeInfo.min + "分" + timeInfo.sec + "秒";
+    yearMonthDate = timeInfo.year + "年" + timeInfo.month + "月" + timeInfo.date + "日" + " (" + wChars + ")";
     showHour.innerHTML = hourMinSec;
     showDay.innerHTML = yearMonthDate;
-})
 
+    const hourEl = document.querySelector(".hour");
+    const minuteEl = document.querySelector(".min");
+    const secondEl = document.querySelector(".sec");
+
+    const hoursForClock = timeInfo.hour >= 13 ? timeInfo.hour % 12 : timeInfo.hour;
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale (hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(timeInfo.min, 0, 59, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(timeInfo.sec, 0, 59, 0, 360)}deg)`;
+});
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
 
 //swiperJsの設定とオプション
 const swiper = new Swiper(".swiper", {
     // Optional parameters
-    direction: 'horizontal',
+    direction: "horizontal",
     loop: true,
     speed: 1000,
     effect: "slide",
@@ -57,18 +80,3 @@ const swiper = new Swiper(".swiper", {
         prevEl: ".swiper-button-prev",
     },
 });
-
-//アナログタイマー作成
-//各要素の取得
-const hourEl = document.querySelector('.hour');
-const minuteEl = document.querySelector('.min');
-const secondEl = document.querySelector('.sec');
-
-const hoursForClock = hour
-
-//針の動き
-// hourEl.style.transform = `translate(-50%, 100%) rotate(${})` //針の角度を時間によって変更する処理を書く
-
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num - in_min)*(out_max - out_min)/(in_max - in_min) + out_min;
-}
